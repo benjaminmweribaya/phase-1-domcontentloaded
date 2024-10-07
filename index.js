1b30367c4e09da9376554a4156e780b3e7a2f4ef
benjaminmweribaya <b3njaminbaya@gmail.com>
2024-10-07 13:41:38 +0300

    The JavaScript DOMContentLoaded Event

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+    const targetElement = document.getElementById("text");
+    targetElement.textContent = "This is really cool!";
+});
